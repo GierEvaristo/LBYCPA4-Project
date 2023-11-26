6922393b59a053efd54bebcd7d186f108e44dc82
gierevaristo <gier_evaristo@dlsu.edu.ph>
2023-11-27 00:53:31 +0800

    89 percent validation
    gegegege

--- project.m
+++ project.m
@@ -1,10 +1,31 @@
-trainingPath = fullfile('shoeTypeClassifierDataset','training');
-validationPath = fullfile('shoeTypeClassifierDataset','validation');
-trainingImds = imageDatastore(trainingPath,'IncludeSubfolders',true,'LabelSource','foldernames');
-validationImds = imageDatastore(validationPath,'IncludeSubfolders',true,'LabelSource','foldernames');
+% trainingPath = fullfile('shoeTypeClassifierDataset','training');
+% validationPath = fullfile('shoeTypeClassifierDataset','validation');
+% trainingImds = imageDatastore(trainingPath,'IncludeSubfolders',true,'LabelSource','foldernames');
+% validationImds = imageDatastore(validationPath,'IncludeSubfolders',true,'LabelSource','foldernames');
 
-trainingImds = augmentedImageDatastore([200 200],trainingImds,'ColorPreprocessing','gray2rgb');
-validationImds = augmentedImageDatastore([200 200],validationImds,'ColorPreprocessing','gray2rgb');
+
+imdsPath = fullfile('ut-zap50k-images-square');
+imds = imageDatastore(imdsPath,'IncludeSubfolders',true,'LabelSource','foldernames');
+
+% Specify the ratios for the train-test-validation split (e.g., 80% train)
+trainRatio = 0.8;
+
+% Perform the train-test split
+[trainingImds, validationImds] = splitEachLabel(imds, trainRatio);
+
+
+augmentation = imageDataAugmenter( ...
+    'RandRotation', [-10, 10], ...  % Random rotation between -10 and 10 degrees
+    'RandXTranslation', [-30, 30], ...  % Random horizontal translation between -30 and 30 pixels
+    'RandYTranslation', [-30, 30], ...  % Random vertical translation between -30 and 30 pixels
+    'RandXScale', [0.8, 1.2], ...  % Random horizontal scaling between 0.8 and 1.2
+    'RandYScale', [0.8, 1.2], ...  % Random vertical scaling between 0.8 and 1.2
+    'RandXShear', [-20, 20], ...  % Random horizontal shear between -20 and 20 degrees
+    'RandYShear', [-20, 20] ...  % Random vertical shear between -20 and 20 degrees
+);
+
+trainingImds = augmentedImageDatastore([136 136],trainingImds,'ColorPreprocessing','gray2rgb','DataAugmentation', augmentation);
+validationImds = augmentedImageDatastore([136 136],validationImds,'ColorPreprocessing','gray2rgb','DataAugmentation', augmentation);
 
 figure;
 perm = randperm(1494,20);
@@ -14,31 +35,44 @@ for i = 1:20
 end
 
 layers = [
-    imageInputLayer([200 200 3])
-
-    convolution2dLayer(3,8,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
-
-    maxPooling2dLayer(2,'Stride',2)
-
-    convolution2dLayer(3,16,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
-
-    maxPooling2dLayer(2,'Stride',2)
-
-    convolution2dLayer(3,32,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
-
-    fullyConnectedLayer(6)
-    softmaxLayer
-    classificationLayer];
+    imageInputLayer([136 136 3]) % Input layer for a 136x136 RGB image
+    
+    convolution2dLayer(11, 96, 'Stride', 4, 'Padding', 0) % Convolutional layer 1
+    reluLayer % ReLU activation layer
+    crossChannelNormalizationLayer(5) % Cross-channel normalization layer
+    maxPooling2dLayer(3, 'Stride', 2) % Max pooling layer
+    
+    convolution2dLayer(5, 256, 'Stride', 1, 'Padding', 2) % Convolutional layer 2
+    reluLayer % ReLU activation layer
+    crossChannelNormalizationLayer(5) % Cross-channel normalization layer
+    maxPooling2dLayer(3, 'Stride', 2) % Max pooling layer
+    
+    convolution2dLayer(3, 384, 'Stride', 1, 'Padding', 1) % Convolutional layer 3
+    reluLayer % ReLU activation layer
+    
+    convolution2dLayer(3, 384, 'Stride', 1, 'Padding', 1) % Convolutional layer 4
+    reluLayer % ReLU activation layer
+    
+    convolution2dLayer(3, 256, 'Stride', 1, 'Padding', 1) % Convolutional layer 5
+    reluLayer % ReLU activation layer
+    maxPooling2dLayer(3, 'Stride', 2) % Max pooling layer
+    
+    fullyConnectedLayer(4096) % Fully connected layer 1
+    reluLayer % ReLU activation layer
+    dropoutLayer(0.5) % Dropout layer for regularization
+    
+    fullyConnectedLayer(4096) % Fully connected layer 2
+    reluLayer % ReLU activation layer
+    dropoutLayer(0.5) % Dropout layer for regularization
+    
+    fullyConnectedLayer(4) % Fully connected layer 3 (output layer)
+    softmaxLayer % Softmax activation layer
+    classificationLayer % Classification layer
+];
 
 options = trainingOptions('sgdm', ...
-    'InitialLearnRate',0.01, ...
-    'MaxEpochs',20, ...
+    'InitialLearnRate',0.001, ...
+    'MaxEpochs',50, ...
     'Shuffle','every-epoch', ...
     'ValidationData',validationImds, ...
     'ValidationFrequency',30, ...
